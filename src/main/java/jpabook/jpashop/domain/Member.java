5cb60f070a7fea5e418420ed430e20ede47283fc
seo <syh05247@naver.com>
2022-12-06 23:00:35 +0900

    JPA 기본 학습

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Member extends BaseEntity{
+
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+
+}
